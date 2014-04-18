f64884f446b2395fbc5e89e0ceb60fe53a6602d2
FObermaier <felix.obermaier@netcologne.de>
2014-04-18 21:51:20 +0000

    [ProjNet4GeoAPI] - The unit test for r1215
    git-svn-id: http://nettopologysuite.googlecode.com/svn/ProjNet4GeoAPI@1216 53ca42c3-9f19-0410-8ccb-b990a9bb5db6

--- ProjNet.Tests/SharpMapIssues.cs
+++ ProjNet.Tests/SharpMapIssues.cs
@@ -34,5 +34,23 @@ namespace ProjNet.UnitTests
             var res = asm.GetType(typeName);
             return res;
         }
+
+        private string wkt7151 = "PROJCS[\"NAD_1983_Hotine_Oblique_Mercator_Azimuth_Natural_Origin\",GEOGCS[\"GCS_North_American_1983\",DATUM[\"D_North_American_1983\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.017453292519943295]],PROJECTION[\"Hotine_Oblique_Mercator\"],PARAMETER[\"longitude_of_center\",-86.0],PARAMETER[\"latitude_of_center\",45.30916666666666],PARAMETER[\"azimuth\",337.25555999999995],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",2546731.496],PARAMETER[\"false_northing\",-4354009.816],PARAMETER[\"rectified_grid_angle\",337.25555999999995],UNIT[\"m\",1.0]]";
+        //projection problem with Michigan GeoRef
+        [Test, Description("projection problem with Michigan GeoRef")]
+        public void TestMichiganGeoRefToWebMercator()
+        {
+            var src = CoordinateSystemFactory.CreateFromWkt(wkt7151);
+            var tgt = CoordinateSystems.ProjectedCoordinateSystem.WebMercator;
+
+            ICoordinateTransformation transform = null;
+            Assert.DoesNotThrow(() => transform = CoordinateTransformationFactory.CreateFromCoordinateSystems(src, tgt));
+            Assert.IsNotNull(transform);
+            var ptSrc = new[] {535247.9375, 324548.09375};
+            double[] ptTgt = null;
+            Assert.DoesNotThrow(() => ptTgt = transform.MathTransform.Transform(ptSrc));
+            Assert.IsNotNull(ptTgt);
+        }
+
     }
 }
